6303dd51a988fa1dad80a456ae5f9e32bf0ef91f
Maalin Ogaja <121969650+maogaja@users.noreply.github.com>
2023-12-10 10:49:19 +0300

    n

---
+++ 8kyu/Fake Binary.js
@@ -0,0 +1,12 @@
+/*Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
+
+Note: input will never be an empty string*/
+
+
+function fakeBin(x){
+    result = '';
+    for (let i of x){
+      (i < 5)? result += '0' : result +='1';
+    }
+    return result;
+  }

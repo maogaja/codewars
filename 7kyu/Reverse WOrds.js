15c41f597b0ab81f66fefc3ae5a3cf86d8c8a48f
Maalin Ogaja <121969650+maogaja@users.noreply.github.com>
2024-01-23 15:15:25 +0300

    okay

---
+++ 7kyu/Reverse WOrds.js
@@ -0,0 +1,10 @@
+/*Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
+"This is an example!" ==> "sihT si na !elpmaxe"
+"double  spaces"      ==> "elbuod  secaps"
+*/
+
+function reverseWords(str) {
+    return str.split(' ').map(function(word){
+    return word.split('').reverse().join('');
+  }).join(' ');
+}

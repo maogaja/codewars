bdd60700541777a6875c49041e978996554e5669
Maalin Ogaja <121969650+maogaja@users.noreply.github.com>
2024-01-26 13:07:04 +0300

    if base can be divided by factor and reminder is zero= factor

---
+++ 8kyu/Grasshopper-Check Factor.js
@@ -0,0 +1,13 @@
+/*This function should test if the factor is a factor of base.
+
+Return true if it is a factor or false if it is not.
+
+About factors
+Factors are numbers you can multiply together to get another number.
+
+2 and 3 are factors of 6 because: 2 * 3 = 6
+
+You can find a factor by dividing numbers. If the remainder is 0 then the number is a factor.*/
+function checkForFactor (base, factor) {
+    return base % factor === 0;
+ }
